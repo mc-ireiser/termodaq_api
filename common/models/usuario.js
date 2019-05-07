af3117b3b8fc70e4380918717e96b753f0137750
Jesus Rodriguez <mc.ireiser@gmail.com>
2019-05-06 23:41:10 -0400

    termoDaQ API

---
+++ common/models/usuario.js
@@ -0,0 +1,33 @@
+'use strict';
+
+var path = require('path');
+
+module.exports = function(Usuario) {
+  //send verification email after registration
+  Usuario.afterRemote('create', function(context, userInstance, next) {
+    console.log('> Usuario.afterRemote triggered');
+
+    var options = {
+      type: 'email',
+      to: userInstance.email,
+      from: process.env.EMAIL_FROM,
+      subject: 'Gracias por registrarte.',
+      template: path.resolve(__dirname, '../templates/email-verify.ejs'),
+      redirect: process.env.FRONT_VERIFIED,
+      user: Usuario
+    };
+
+    userInstance.verify(options, function(err, response, next) {
+      if (err) return next(err);
+
+      console.log('> verification email sent:');
+      console.log(response);
+
+      context.res.send(userInstance);
+    });
+
+  });
+};
+
+
+
---
+++ server/datasources.local.js
@@ -0,0 +1,35 @@
+/* eslint-disable camelcase */
+/* eslint-disable comma-dangle */
+
+'use strict';
+
+module.exports = {
+  db_mongo: {
+    connector: 'mongodb',
+    name: 'db_mongo',
+    url: process.env.DB_URL || '',
+    host: process.env.DB_HOST || 'localhost',
+    port: process.env.DB_PORT || 27017,
+    database: process.env.DB_NAME || 'termodaq',
+    user: process.env.DB_USER || '',
+    password: process.env.DB_PASS || ''
+  },
+
+  email: {
+    name: 'email',
+    connector: 'mail',
+    transports: [{
+      type: process.env.EMAIL_TYPE,
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: false,
+      tls: {
+        rejectUnauthorized: false
+      },
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS
+      }
+    }]
+  }
+};
